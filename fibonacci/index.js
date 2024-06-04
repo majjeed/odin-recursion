cf666b4ab1ceb5f94a5985003d22fd16552dfebf
majjeed <abdulmajeed.hersi@gmail.com>
2024-06-04 15:11:39 +0100

    complete fibsRec

--- fibonacci/index.js
+++ fibonacci/index.js
@@ -1,16 +1,32 @@
 function fibs(n) {
-  if (n < 2) return n;
-  else {
-    return fibs(n - 1) + fibs(n - 2);
+  let fibSequence = [0, 1];
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+  for (let i = 2; i < n; i++) {
+    fibSequence.push(fibSequence[i - 1] + fibSequence[i - 2]);
   }
+
+  return fibSequence;
 }
 
-function arrFibs(n) {
-  let arr = [];
-  for (let i = 0; i < n; i++) {
-    arr.push(fibs(i));
+console.log("fibs: ", fibs(8));
+
+function fibsRec(n) {
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+
+  function fibHelper(num) {
+    if (num <= 0) return [0];
+    if (num === 1) return [0, 1];
+
+    const sequence = fibHelper(num - 1);
+    sequence.push(
+      sequence[sequence.length - 1] + sequence[sequence.length - 2]
+    );
+    return sequence;
   }
-  return arr;
+
+  return fibHelper(n - 1);
 }
 
-console.log(arrFibs(1));
+console.log("fibsRec: ", fibsRec(8));
