067f6ad5c626f8e12f7e14fb0c9f31b8428312c4
majjeed <abdulmajeed.hersi@gmail.com>
2024-06-04 17:15:36 +0100

    working mergesort

--- merge_sort/index.js
+++ merge_sort/index.js
@@ -1,20 +1,36 @@
+function merge(left, right) {
+  let resultArray = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      resultArray.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      resultArray.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return resultArray
+    .concat(left.slice(leftIndex))
+    .concat(right.slice(rightIndex));
+}
+
 function mergeSort(arr) {
   //if only one element in array then its sorted
   //sort left half of nums
   //sort right half of nums
   //merge the sorted halves
-  let emptArr = [];
-  if (arr.length === 0) return [];
-  if (arr.length === 1) return arr[0];
+  if (arr.length === 1) return arr;
+
   let midPoint = Math.floor(arr.length / 2);
   let leftArr = arr.slice(0, midPoint);
   let rightArr = arr.slice(midPoint, arr.length);
-  let a = mergeSort(leftArr);
-  let b = mergeSort(rightArr);
-  if (a < b) return emptArr.push(a);
-  return emptArr.push(b);
+
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
 }
 
 let sortedArr = [1, 2, 3, 4, 5];
-let unsortedArr = [5, 2, 1, 4, 3];
-console.log(unsortedArr);
+let unsortedArr = [5, 2, 1, 4, 3, 8, 9];
+console.log(mergeSort(unsortedArr));
